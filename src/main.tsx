0ddd5d8caf1e6ee94d070270946bf022cb8ec20d
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-09-02 10:34:05 -0300

    Make library installable as PWA

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,9 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/BIBLIOCPUBAT15/sw.js').catch(() => undefined);
+  });
+}
